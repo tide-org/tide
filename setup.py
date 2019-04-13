2e63d0695c21a2957bd49962f7f0f21974bf02c7
Willem van Ketwich <willvk@gmail.com>
2019-04-13 11:32:20 +1000

    fix jinja

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
   name = 'tide',
-  version = '0.1',
+  version = '0.1.1',
   description = 'From text editor to Integrated Development Environment. This allows you to use a yaml config and some scripting to deliver cross-platform, cross-editor plugins.',
   author = 'Willem van Ketwich',
   author_email = 'willvk@gmail.com',
@@ -38,7 +38,7 @@ setuptools.setup(
   keywords = ['editor', 'configuration'],
   packages=setuptools.find_packages(),
   install_requires = [
-      'jinja',
+      'jinja2',
       'markupsafe',
       'pexpect',
       'ptyprocess',
