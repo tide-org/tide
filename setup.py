adc29758b56b693929167c41d489582c1b384209
Willem van Ketwich <willvk@gmail.com>
2019-06-19 21:52:34 +1000

    make import from tide instead of tide.tide

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 """
 A package for adding enhancements to text editors in a cross-platform, cross-editor manner.
 https://github.com/wilvk/tide
+
+See:
+https://github.com/wilvk/tide
+
 """
 
-import setuptools
+from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
@@ -12,17 +16,27 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setuptools.setup(
+setup(
+
   name = 'tide',
-  version = '0.1.6',
+
+  version = '0.1.8',
+
   description = 'From text editor to Integrated Development Environment. This allows you to use a yaml config and some scripting to deliver cross-platform, cross-editor plugins.',
+
   author = 'Willem van Ketwich',
   author_email = 'willvk@gmail.com',
+
   license = 'MIT',
+
   python_requires='>=3.4',
+
   url = 'https://github.com/wilvk/tide',
+
   download_url = 'https://github.com/wilvk/tide/archive/0.1.tar.gz',
+
   long_description=long_description,
+
   classifiers=[
       'Development Status :: 3 - Alpha',
       'Intended Audience :: Developers',
@@ -35,9 +49,9 @@ setuptools.setup(
       'Intended Audience :: Information Technology',
       'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
   ],
+
   keywords = ['editor', 'configuration'],
-  packages = setuptools.find_packages(),
-  include_package_data=True,
+
   install_requires = [
       'jinja2',
       'markupsafe',
@@ -47,8 +61,18 @@ setuptools.setup(
       'six',
       'yamlreader'
   ],
+
+  packages = find_packages(),
+
   extras_require={
       'dev': [],
       'test': [ 'nose' ],
   },
+
+  entry_points={
+      'console_scripts': [
+          'tide=tide:main',
+      ],
+  },
+
 )
--- tide/__init__.py
+++ tide/__init__.py
@@ -2,3 +2,5 @@
 '''
     __init__.py
 '''
+
+from .tide import Tide
