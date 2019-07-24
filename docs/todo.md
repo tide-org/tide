94a0d3cd021059d62d9a364767ab8ea63d48907b
Willem van Ketwich <willvk@gmail.com>
2019-07-24 21:26:47 +1000

    initial commit

---
+++ docs/todo.md
@@ -0,0 +1,2 @@
+[ ] local server for IDEs that don't have python integrated
+[ ] setup tutorial
--- tide/__init__.py
+++ tide/__init__.py
@@ -4,3 +4,25 @@
 '''
 
 from .tide import Tide
+import argparse
+import os
+
+def main():
+    '''
+       Main entrypoint for the cli application
+    '''
+
+    parser = argparse.ArgumentParser(description='Tide. From Text to Integrated Development Environment.')
+    parser.add_argument('-c', '--config', help='The path of the config file')
+    parser.add_argument('-a', '--args', help='A string of command arguments to be sent to Tide on first startup.')
+    args = vars(parser.parse_args())
+    arg_string = ''
+
+    if args['config']:
+        os.environment["TIDE_CONFIG_LOCATION"] = args['config'] 
+
+    if args['args']:
+        arg_string = args['args']
+
+    tide = Tide()
+    tide.start(arg_string)
