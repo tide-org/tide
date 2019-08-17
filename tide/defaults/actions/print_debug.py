66e2d8217ee5b4cb6a5b414429b9f965587c761e
Willem van Ketwich <willvk@gmail.com>
2019-08-17 18:55:28 +1000

    add print features

--- tide/defaults/actions/print_debug.py
+++ tide/defaults/actions/print_debug.py
@@ -1,12 +1,21 @@
 from action_base import action_base
 import interpolate as Interpolate
+from config import Config
+import json
+import sys
 
 class print_debug(action_base):
 
     def run(self, command_item, buffer_name=''):
         msg = command_item.get("msg", '')
         interpolated_message = Interpolate.interpolate_variables(msg)
-        print_buffer_name = command_item.get("buffer_name", 0)
-        if print_buffer_name:
-            print("print_debug - buffer name: " + buffer_name)
-        print("print_debug - " + str(interpolated_message))
+        buffer_name = command_item.get("buffer_name", 0)
+        all_config = command_item.get("all_config", 0)
+        print("print_debug:")
+        if all_config:
+            print("  all_config:\n  ")
+            json.dump(Config().get(), sys.stdout, indent=4)
+        if buffer_name:
+            print("  buffer name:\n  ")
+            json.dump(Config().get()["buffers"][buffer_name], sys.stdout, indent=4)
+        print("" + str(interpolated_message))
