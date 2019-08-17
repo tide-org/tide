8f4e6bf0e1655dcac81b6c3243af95f18e1ef06c
Willem van Ketwich <willvk@gmail.com>
2019-08-17 18:53:16 +1000

    refactor, add logging

--- tests/nose_tests/setup_tests.py
+++ tests/nose_tests/setup_tests.py
@@ -4,13 +4,24 @@ import sys
 
 current_dir = dirname(abspath(inspect.getfile(inspect.currentframe())))
 tide_dir = join(current_dir, "../../tide")
-paths_list = [current_dir, tide_dir]
-defaults_list = ['actions', 'editor_wrappers', 'filters', 'functions']
+paths_list = [
+    current_dir,
+    tide_dir
+]
+
+defaults_list = [
+    'actions',
+#    'editor_wrappers',
+    'filters',
+    'functions',
+    '.'
+]
 
 for default in defaults_list:
     paths_list.append(join(tide_dir, "defaults", default))
 
 for insert_path in paths_list:
-    sys.path.insert(0, insert_path)
+    print("inserting into sys.path: " + abspath(insert_path))
+    sys.path.insert(0, abspath(insert_path))
 
 import lib_paths
