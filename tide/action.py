a1a4da2bfc8ce4a8bf4e54b4b071c42c25fba16b
Willem van Ketwich <willvk@gmail.com>
2019-08-17 18:54:46 +1000

    improve error messages. fix bug where if path already exists in sys.path it was not getting action files

--- tide/action.py
+++ tide/action.py
@@ -19,7 +19,7 @@ def __get_actions_list():
         action_files = [f for f in listdir(actions_path) if isfile(join(actions_path, f))]
         if action_files and actions_path not in sys.path:
             sys.path.insert(0, actions_path)
-            all_action_files.extend(action_files)
+        all_action_files.extend(action_files)
     for action_file in all_action_files:
         if Path(action_file).suffix.lower() == ".py" and action_file.lower() != "__init__.py":
             actions_list.append(Path(action_file).stem.lower())
@@ -34,7 +34,8 @@ def run_action(action_name, args_dict):
     if action_name.lower() in ACTIONS_LIST:
         return __call_action_class(action_name, args_dict)
     else:
-        raise TypeError("error: action: " + action_name + " is not a valid action")
+
+        raise TypeError("error: action: " + action_name + " is not a valid action in ACTIONS_LIST: " + str(ACTIONS_LIST))
 
 @logging
 def __call_action_class(action_name, args_dict):
