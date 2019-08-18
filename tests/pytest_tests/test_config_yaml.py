5f6940ff522173d623decfe3b799887d5a5f499f
Willem van Ketwich <willvk@gmail.com>
2019-08-18 15:32:55 +1000

    add tests - one failing

--- tests/pytest_tests/test_config_yaml.py
+++ tests/pytest_tests/test_config_yaml.py
@@ -45,6 +45,35 @@ def test_tide_can_start_and_command_has_action_key(capsys):
     assert "action" in json_object["command"].keys()
     del Tide
 
+def test_tide_can_start_and_command_has_value_key(capsys):
+    from tide import Tide
+    tide_object = Tide()
+    tide_object.start()
+    capture = capsys.readouterr()
+    json_object = json.loads(capture.out)
+    assert "value" in json_object["command"].keys()
+    del Tide
+
+def test_tide_can_start_and_value_has_config_dictionary_key(capsys):
+    from tide import Tide
+    tide_object = Tide()
+    tide_object.start()
+    capture = capsys.readouterr()
+    json_object = json.loads(capture.out)
+    assert "config_dictionary" in json_object["command"]["value"].keys()
+    del Tide
+
+def test_tide_can_start_and_config_dictionary_has_settings_key(capsys):
+    from tide import Tide
+    tide_object = Tide()
+    tide_object.start()
+    capture = capsys.readouterr()
+    json_object = json.loads(capture.out)
+    with capsys.disabled():
+        print("CONFIG_DICTIONARY::" + str(json_object["command"]["value"]["config_dictionary"]["internal"].keys()))
+    assert "settings" in json_object["command"]["value"]["config_dictionary"].keys()
+    del Tide
+
 def test_tide_can_output_to_stdout(capsys):
     from tide import Tide
     tide_object = Tide()
