0e61a4381b2e96d9c77cfa402dd8e2904aea9f22
Willem van Ketwich <willvk@gmail.com>
2019-08-31 00:01:47 +1000

    remove unused thread

--- tide/defaults/functions/thread_wrapper.py
+++ tide/defaults/functions/thread_wrapper.py
@@ -1,7 +1,7 @@
-import time
+import fileinput
+import os
 from threading import Thread
 from message_container import MessageContainer
-import fileinput
 
 def stdin_loop(messages):
     while True:
@@ -9,21 +9,12 @@ def stdin_loop(messages):
             if line:
                 messages.add_message(line)
 
-def tide_api_message_loop(messages):
-    while True:
-        api_message = messages.pop_tide_api_message()
-        if api_message:
-            # TODO: call run_config_command here
-            print("MESSAGE:" + str(api_message))
-
 class ThreadWrapper:
 
     def __init__(self):
         self.message_container = MessageContainer()
         self.stdin_thread = Thread(target=stdin_loop, args=(self.message_container,))
         self.stdin_thread.start()
-        self.api_thread = Thread(target=tide_api_message_loop, args=(self.message_container,))
-        self.api_thread.start()
 
     def get_message_by_key(self, key):
         while True:
