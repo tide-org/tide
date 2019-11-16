ce1e701dd36bd6ab4690bb53a34fa0d0b7c39b3e
Willem van Ketwich <willvk@gmail.com>
2019-11-16 21:22:04 +1100

    fix tests

--- tests/pytest_tests/test_config_yaml.py
+++ tests/pytest_tests/test_config_yaml.py
@@ -2,105 +2,96 @@ import os
 import pytest
 import json
 import setup_tests
-from timeout import Timeout
 
-os.environ["TIDE_CONFIG_LOCATION"] = "/work/plugins/tests/test_hello"
+os.environ["TIDE_CONFIG_LOCATION"] = "/work/plugins/tests/test_hello_3"
 
 timeout_seconds = 1
 tide_object = None
 
-#def test_object_is_type_tide():
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#    except Timeout.Timeout:
-#        assert type(tide_object) == Tide
-#
-#def test_tide_can_start():
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#            tide_object.start()
-#    except Timeout.Timeout:
-#        tide_object.stop()
-#        assert type(tide_object) == Tide
-#
-#def test_tide_can_start_and_returns_json(capsys):
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#            tide_object.start()
-#    except Timeout.Timeout:
-#        tide_object.stop()
-#        capture = capsys.readouterr()
-#        json_object = json.loads(capture.out)
-#        assert type(json_object) == dict
-#
-#def test_tide_can_start_and_json_has_command_key(capsys):
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#            tide_object.start()
-#    except Timeout.Timeout:
-#        tide_object.stop()
-#        capture = capsys.readouterr()
-#        json_object = json.loads(capture.out)
-#        assert "command" in json_object.keys()
-#
-#def test_tide_can_start_and_command_has_action_key(capsys):
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#            tide_object.start()
-#    except Timeout.Timeout:
-#        tide_object.stop()
-#        capture = capsys.readouterr()
-#        json_object = json.loads(capture.out)
-#        assert "action" in json_object["command"].keys()
-#
-#def test_tide_can_start_and_command_has_value_key(capsys):
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#            tide_object.start()
-#    except Timeout.Timeout:
-#        tide_object.stop()
-#        capture = capsys.readouterr()
-#        json_object = json.loads(capture.out)
-#        assert "value" in json_object["command"].keys()
-#
-#def test_tide_can_start_and_value_has_config_dictionary_key(capsys):
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#            tide_object.start()
-#    except Timeout.Timeout:
-#        tide_object.stop()
-#        capture = capsys.readouterr()
-#        json_object = json.loads(capture.out)
-#        assert "config_dictionary" in json_object["command"]["value"].keys()
-#
-#def test_tide_can_start_and_config_dictionary_has_settings_key(capsys):
-#    try:
-#        with Timeout(timeout_seconds):
-#            from tide import Tide
-#            tide_object = Tide()
-#            tide_object.start()
-#    except Timeout.Timeout:
-#        tide_object.stop()
-#        capture = capsys.readouterr()
-#        json_object = json.loads(capture.out)
-#        with capsys.disabled():
-#            print("CONFIG_DICTIONARY::" + str(json_object["command"]["value"]["config_dictionary"]["internal"].keys()))
-#        assert "settings" in json_object["command"]["value"]["config_dictionary"].keys()
-#
+
+class TestConfigYaml():
+
+    def test_tide_can_start_and_has_9_lines(self, capsys):
+        from tide import Tide
+        tide_object = Tide()
+        tide_object.start()
+        tide_object.stop()
+        capture = capsys.readouterr()
+        out_string = capture.out
+        assert len(out_string.split("\n")) == 9
+
+    def test_tide_can_start_and_has_json_object(self, capsys):
+        from tide import Tide
+        tide_object = Tide()
+        tide_object.start()
+        tide_object.stop()
+        capture = capsys.readouterr()
+        out_string = capture.out
+        out_strings = out_string.split("\n")
+        json_string = out_strings[0]
+        json_object = json.loads(json_string)
+        assert(isinstance(json_object, dict))
+
+    def test_tide_can_start_and_has_json_object_with_command_key(self, capsys):
+        from tide import Tide
+        tide_object = Tide()
+        tide_object.start()
+        tide_object.stop()
+        capture = capsys.readouterr()
+        out_string = capture.out
+        out_strings = out_string.split("\n")
+        json_string = out_strings[0]
+        json_object = json.loads(json_string)
+        assert "command" in json_object.keys()
+
+    def test_tide_can_start_and_command_has_action_key(self, capsys):
+        from tide import Tide
+        tide_object = Tide()
+        tide_object.start()
+        tide_object.stop()
+        capture = capsys.readouterr()
+        out_string = capture.out
+        out_strings = out_string.split("\n")
+        json_string = out_strings[0]
+        json_object = json.loads(json_string)
+        assert "action" in json_object["command"].keys()
+
+    def test_tide_can_start_and_command_has_value_key(self, capsys):
+        from tide import Tide
+        tide_object = Tide()
+        tide_object.start()
+        tide_object.stop()
+        capture = capsys.readouterr()
+        out_string = capture.out
+        out_strings = out_string.split("\n")
+        json_string = out_strings[0]
+        json_object = json.loads(json_string)
+        assert "value" in json_object["command"].keys()
+
+    def test_tide_can_start_and_value_has_config_dictionary_key(self, capsys):
+        from tide import Tide
+        tide_object = Tide()
+        tide_object.start()
+        tide_object.stop()
+        capture = capsys.readouterr()
+        out_string = capture.out
+        out_strings = out_string.split("\n")
+        json_string = out_strings[0]
+        json_object = json.loads(json_string)
+        assert "config_dictionary" in json_object["command"]["value"].keys()
+
+    def test_tide_can_start_and_config_dictionary_has_internal_key(self, capsys):
+        from tide import Tide
+        tide_object = Tide()
+        tide_object.start()
+        tide_object.stop()
+        capture = capsys.readouterr()
+        out_string = capture.out
+        out_strings = out_string.split("\n")
+        json_string = out_strings[0]
+        json_object = json.loads(json_string)
+        assert "internal" in json_object["command"]["value"]["config_dictionary"].keys()
+
 #def test_tide_can_output_to_stdout(capsys):
 #    try:
 #        with Timeout(timeout_seconds):
