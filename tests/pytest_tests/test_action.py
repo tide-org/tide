4839e300f42ed71d338b4068cd4c3fccc34ae401
Willem van Ketwich <willvk@gmail.com>
2019-12-15 00:15:42 +1100

    add action tests

---
+++ tests/pytest_tests/test_action.py
@@ -0,0 +1,22 @@
+import os
+import setup_tests
+import pytest
+
+os.environ["TIDE_CONFIG_LOCATION"] = "/work/plugins/tests/test_hello_3"
+
+import action as ACTION
+
+
+def test__get_actions_list__has_correct_count():
+    actions_list = ACTION.__get_actions_list()
+    assert len(actions_list) == 11
+
+def test__get_actions_list__has_expected_actions():
+    actions_list = ACTION.__get_actions_list()
+    assert set(actions_list) == set(['run_config_command', 'run_command_with_match', 'run_command',
+                                     'get_current_buffer_line', 'run_editor_function', 'run_command_string',
+                                     'print_debug', 'display_template', 'get_current_buffer_name',
+                                     'run_python_function', 'set_var'])
+
+def test__call_action_class__can_call_class():
+    pass
