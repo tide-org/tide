c70b30bc9f9bbdbc8d321b35c846a32fb1d9693b
Willem van Ketwich <willvk@gmail.com>
2019-12-15 12:02:04 +1100

    add a test for calling an action"

--- tests/pytest_tests/test_action.py
+++ tests/pytest_tests/test_action.py
@@ -18,5 +18,12 @@ def test__get_actions_list__has_expected_actions():
                                      'print_debug', 'display_template', 'get_current_buffer_name',
                                      'run_python_function', 'set_var'])
 
-def test__call_action_class__can_call_class():
-    pass
+def test__call_action_class__can_call_class(capsys):
+    action_name = "print_debug"
+    test_message = 'this is a test message'
+    args_dict = {'command_item': {'msg': test_message } }
+    action_result = ACTION.__call_action_class(action_name, args_dict)
+    capture = capsys.readouterr().out
+    capture_lines = capture.split("\n")
+    assert capture_lines[-2] == test_message
+
