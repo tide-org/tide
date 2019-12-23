556b2bc616e98591c1aa2d4686e1e8055c018278
Willem van Ketwich <willvk@gmail.com>
2019-12-23 19:06:45 +1100

    fix paths in tests

--- tests/pytest_tests/setup_tests.py
+++ tests/pytest_tests/setup_tests.py
@@ -1,13 +1,10 @@
-import os
 from os.path import join, dirname, abspath
 import inspect
 import sys
 
 current_dir = dirname(abspath(inspect.getfile(inspect.currentframe())))
-tide_dir = join(current_dir, "../../tide")
-os.chdir(tide_dir)
+tide_dir = abspath(join(current_dir, "../../tide"))
 paths_list = [current_dir, tide_dir]
-paths_list = []
 
 for default in ['actions', 'editor_wrappers', 'filters', 'functions', '.']:
     paths_list.append(join(tide_dir, "defaults", default))
