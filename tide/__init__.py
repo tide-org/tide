5bbe921c0d83757612b587e1be10b95269b9ee8b
Willem van Ketwich <willvk@gmail.com>
2019-12-23 17:48:26 +1100

    remove cruft from __init__.py

--- tide/__init__.py
+++ tide/__init__.py
@@ -1,37 +1,11 @@
-# -*- coding: utf-8 -*-
-'''
-    __init__.py
-'''
-
 import os
-import sys
 from time import sleep
-from os.path import dirname, abspath, join
 import argparse
 from argparse import RawTextHelpFormatter
 from threading import Thread
 import asyncio
 from .tide import Tide
 from .config import Config
-
-def call_on_startup_functions(tide):
-    pass
-
-def check_update_buffers(command):
-    pass
-
-def run_config_command(tide, command):
-    tide.run_config_command(command)
-    print("config command ran successfully: " + command)
-    check_update_buffers(command)
-
-def run_startup_commands(tide, position):
-    if "events" in Config().get():
-        startup_commands = Config().get()["events"].get(position + "_startup", [])
-        if startup_commands:
-            for startup_command in startup_commands:
-                run_config_command(tide, startup_command)
-
 
 def start_loop_seconds(loop, timeout_seconds):
     future = asyncio.wait_for(loop.run_in_executor(None, sleep, timeout_seconds), None)
@@ -44,77 +18,24 @@ def start_loop(loop):
 def start_new_loop(timeout_seconds):
     new_loop = asyncio.new_event_loop()
     if not timeout_seconds:
-        t = Thread(target=start_loop, args=(new_loop, ))
+        thread = Thread(target=start_loop, args=(new_loop, ))
     else:
-        t = Thread(target=start_loop_seconds, args=(new_loop, timeout_seconds, ))
-    t.start()
+        thread = Thread(target=start_loop_seconds, args=(new_loop, timeout_seconds, ))
+    thread.start()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tide - From Text editor to Integrated Development Environment', formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-c', '--config', help='The path to the Tide Config file(s).', metavar=('config'), required=False)
+    parser.add_argument('-e', '--editor', help='The name of the edotor wrapper to target.', metavar=('editor'), required=False)
+    parser.add_argument('-t', '--timeout', help="A timeout value in seconds to specify for how long\nthe main thread will stay alive for. If not specified,\nwill default to infinite.", metavar=('timeout'), required=False)
+    parser.add_argument('-a', '--arguments', help="A quoted string of arguments to pass \nto Tide on startup.\ne.g. tide -a \'test.exe 1 2 3\'", metavar=('args'), required=False)
+    parser.add_argument('-v','--variables', action='append', nargs=2, metavar=('key','value'), help="Used to specify config variables to be\nadded/replaced to the configuration.\nCan be called multiple times.\nConfig key names can be dot-chained.\ne.g. `-v settings.editor.name stdio`\n", required=False)
+    return parser.parse_args()
 
 def main():
-    '''
-       Main entrypoint for the cli application
-    '''
-
-    parser = argparse.ArgumentParser(
-        description='Tide - From Text editor to Integrated Development Environment',
-        formatter_class=RawTextHelpFormatter
-    )
-    parser.add_argument('-c', '--config',
-        help='The path to the Tide Config file(s).',
-        metavar=('config'),
-        required=False
-    )
-    parser.add_argument('-t', '--timeout',
-        help="A timeout value in seconds to specify for how long\n" \
-              "the main thread will stay alive for. If not specified,\n" \
-              "will default to infinite.",
-        metavar=('timeout'),
-        required=False
-    )
-    parser.add_argument('-a', '--arguments',
-        help="A quoted string of arguments to pass \n" \
-             "to Tide on startup.\n" \
-             "e.g. tide -a \'test.exe 1 2 3\'",
-        metavar=('args'),
-        required=False
-    )
-    parser.add_argument('-v','--variables',
-        action='append',
-        nargs=2,
-        metavar=('key','value'),
-        help="Used to specify config variables to be\n" \
-             "added/replaced to the configuration.\n" \
-             "Can be called multiple times.\n" \
-             "Config key names can be dot-chained.\n" \
-             "e.g. `-v settings.editor.name stdio`\n",
-        required=False
-    )
-
-    args = vars(parser.parse_args())
-    tide_args = ''
-
-    if args["config"]:
-        os.environ["TIDE_CONFIG_LOCATION"] = args["config"]
-
-    if args["arguments"]:
-        tide_args = args["arguments"]
-    
-    print("tide config location: " + os.environ.get("TIDE_CONFIG_LOCATION"))
-
-    if args["variables"]:
-        print("Additional config variables: " + str(args["variables"]))
-
-    if args["timeout"]:
-        print("Starting Tide for " + args["timeout"] + " seconds.")
-    else:
-        print("Starting Tide into perpetuity.")
-    sleep_seconds = int(args["timeout"] or 0)
-    start_new_loop(sleep_seconds)
-
-    print("initialising Tide...")
-
-    tide = Tide()
-
-    print("staring Tide with arguments: \'" + tide_args + "\'...")
-    tide.start(tide_args)
-    call_on_startup_functions(tide)
-    run_startup_commands(tide, "after")
+    args = parse_args()
+    if args.config:
+        os.environ['TIDE_CONFIG_LOCATION'] = args.config
+    start_new_loop(int(args.timeout or 0))
+    tide = Tide(args.editor or '')
+    tide.start()
