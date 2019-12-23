542c6104b2219bc056ff1a09a73699fd47a06f4e
Willem van Ketwich <willvk@gmail.com>
2019-12-23 20:06:31 +1100

    tidy up to use config()

--- tide/config_command_buffer_cache.py
+++ tide/config_command_buffer_cache.py
@@ -15,14 +15,14 @@ class ConfigCommandBufferCache(object):
                 internal_buffer_name = self.__get_internal_buffer_name(action_args)
             else:
                 internal_buffer_name = config_command_item.buffer_name
-            lines = self.__set_lines_on_no_buffer_name(internal_buffer_name, lines, command_action)
-            Config().get_internal_buffer_caches()[internal_buffer_name] = lines
+            lines = self.__set_lines_where_no_buffer_name(internal_buffer_name, lines, command_action)
+            Config().set_internal_buffer_cache(internal_buffer_name, lines)
 
     def __initialise_buffer_in_config(self, buffer_name):
         if buffer_name not in Config().get_internal_buffer_caches(): 
             if not Config().get_internal_buffer_caches():
-                Config().get()["internal"]["buffer_caches"] = {}
-            Config().get_internal_buffer_caches()[buffer_name] = []
+                Config().set_internal("buffer_caches", {})
+            Config().set_internal_buffer_cache(buffer_name, [])
 
     def __get_internal_buffer_name(self, action_args):
         buffer_name = action_args.get("command_item", {}).get("buffer_name", "")
@@ -30,7 +30,7 @@ class ConfigCommandBufferCache(object):
             return buffer_name
         return self.__default_buffer_name
 
-    def __set_lines_on_no_buffer_name(self, internal_buffer_name, lines, command_action):
+    def __set_lines_where_no_buffer_name(self, internal_buffer_name, lines, command_action):
         if internal_buffer_name == self.__default_buffer_name:
             error_line = self.__error_line.format(ca_type=command_action.action_name, ca_dict=str(command_action.__dict__))
             if isinstance(lines, str):
