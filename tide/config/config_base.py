cdd282ec04236f6eca3951900fc0d60458700a8c
Willem van Ketwich <willvk@gmail.com>
2019-12-25 23:48:20 +1100

    refactor config_base to be more concise

--- tide/config/config_base.py
+++ tide/config/config_base.py
@@ -8,18 +8,13 @@ class ConfigBase:
 
     def __init__(self, editor_wrapper_name=''):
         self._config_dictionary = None
-        self._editor_wrapper = None
-        self.__editor_wrapper_name = editor_wrapper_name if editor_wrapper_name else Cs.CONFIG_OBJECT["settings"]["editor"]["name"]
-        self.__set_editor_wrapper()
+        self.__editor_wrapper_name = editor_wrapper_name or Cs.CONFIG_OBJECT["settings"]["editor"]["name"]
+        self._editor_wrapper = self._editor_wrapper if hasattr(self, "_editor_wrapper") else EditorWrapper(self.__editor_wrapper_name)
         self.__set_config_dictionary()
 
     def __set_config_dictionary(self):
         if not self._config_dictionary:
             self.set()
-
-    def __set_editor_wrapper(self):
-        if not self._editor_wrapper:
-            self._editor_wrapper = EditorWrapper(self.__editor_wrapper_name)
 
     def get(self):
         self.__set_config_dictionary()
@@ -29,11 +24,8 @@ class ConfigBase:
         if force or not self._config_dictionary:
             callback = self._editor_wrapper.get_set_dictionary_value_callback()
             self._config_dictionary = ActionableDict(Cs.CONFIG_OBJECT, callback)
-            self.__set_editor_dictionary_defaults()
+            self._editor_wrapper.set_editor_dictionary(self._config_dictionary)
             self.__set_internals()
-
-    def __set_editor_dictionary_defaults(self):
-        self._editor_wrapper.set_editor_dictionary(self._config_dictionary)
 
     def __set_internals(self):
         session_buffer = self.get_setting("logging", "session_buffer_name") or "no_session_buffer_name_set"
