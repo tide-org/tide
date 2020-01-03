f67ef5f988dca539f8d20aa8af408b76f2eed204
Willem van Ketwich <willvk@gmail.com>
2020-01-03 21:26:00 +1100

    add tests

--- tests/pytest_tests/test_command_handler.py
+++ tests/pytest_tests/test_command_handler.py
@@ -6,10 +6,85 @@ import pytest
 
 from tide.config.config import Config
 from tide.command.command_handler import CommandHandler
+from tide import Tide
 
 
 def test__can_spawn_process():
     try:
-        pass
+        command_handler = CommandHandler()
+        command_handler.spawn_process()
+        assert command_handler is not None
+    except Exception as ex:
+        pytest.fail("error initialising CommandHandler: " + str(ex))
+
+def test__can_spawn_process_with_startup_command():
+    try:
+        command_handler = CommandHandler()
+        command_handler.spawn_process('echo 1')
+        assert command_handler is not None
+    except Exception as ex:
+        pytest.fail("error initialising CommandHandler: " + str(ex))
+
+def test__can_run_command():
+    try:
+        command_handler = CommandHandler()
+        command_handler.spawn_process()
+        command_handler.run_command("test_command")
+        assert command_handler is not None
+    except Exception as ex:
+        pytest.fail("error initialising CommandHandler: " + str(ex))
+
+def test__can_run_command_and_return_lines():
+    try:
+        command_handler = CommandHandler()
+        command_handler.spawn_process()
+        result = command_handler.run_command("echo hi")
+        assert result == 'echo hi\r\nhi\r\n/work/tide/tests/pytest_tests # \x1b[6n'
+    except Exception as ex:
+        pytest.fail("error initialising CommandHandler: " + str(ex))
+
+def test__can_run_command_and_set_session_log_buffer():
+    try:
+        from tide.config.config import Config
+        command_handler = CommandHandler()
+        command_handler.spawn_process()
+        result = command_handler.run_command("echo hi")
+        buffer_result = Config().get_internal_buffer_caches()
+        assert buffer_result.get("vg_session_log") is not None
+    except Exception as ex:
+        pytest.fail("error initialising CommandHandler: " + str(ex))
+
+def test__can_run_command_and_set_test_buffer():
+    try:
+        test_tide = Tide()
+        command_handler = CommandHandler()
+        command_handler.spawn_process()
+        buffer_result = Config().get_internal_buffer_caches()
+        test_tide.run_config_command('test_command')
+        assert "test_buffer" in buffer_result.keys()
+    except Exception as ex:
+        pytest.fail("error initialising CommandHandler: " + str(ex))
+
+def test__can_run_command_and_set_test_buffer_has_value():
+    try:
+        test_tide = Tide()
+        command_handler = CommandHandler()
+        command_handler.spawn_process()
+        buffer_result = Config().get_internal_buffer_caches()
+        test_tide.run_config_command('test_command')
+        assert len(buffer_result["test_buffer"]) == 3
+    except Exception as ex:
+        pytest.fail("error initialising CommandHandler: " + str(ex))
+
+def test__can_run_command_and_set_test_buffer_has_value_of_line():
+    try:
+        test_tide = Tide()
+        command_handler = CommandHandler()
+        command_handler.spawn_process()
+        buffer_result = Config().get_internal_buffer_caches()
+        test_tide.run_config_command('test_command')
+        assert buffer_result["test_buffer"][0] == 'echo "hello"\r'
+        assert buffer_result["test_buffer"][1] == 'hello\r'
+        assert buffer_result["test_buffer"][2] == '/work/tide/tests/pytest_tests # \x1b[6n'
     except Exception as ex:
         pytest.fail("error initialising CommandHandler: " + str(ex))
