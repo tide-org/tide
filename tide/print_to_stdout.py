8f961fa3dbf340e48c50c524d8d89989ea2461de
Willem van Ketwich <willvk@gmail.com>
2020-01-19 14:10:23 +1100

    remove colour output

--- tide/print_to_stdout.py
+++ tide/print_to_stdout.py
@@ -1,24 +1,16 @@
-from tide.colors import AsciiColors as AC
 from tide.config.config import Config
 
 ljust = True
-
-try:
-    colourise = False
-    import vim
-except:
-    colourise = True
-    
 
 class PrintToStdout:
 
     def info(command_type, command, buffer_name, value):
         if Config().get_setting("debugging", "print_to_stdout"):
-            heading_text = (AC.HEADER if colourise else "") + "[INFO]"
-            command_type_text = (AC.OKGREEN if colourise else "") + "[" + str(command_type) + "]"
-            command_text = (AC.OKBLUE if colourise else "") + "[" + str(command) + "]"
-            buffer_text  = (AC.BOLD if colourise else "") + "[" + str(buffer_name) + "]"
-            value_text = (AC.ENDC if colourise else "") + str(value)
+            heading_text = "[INFO]"
+            command_type_text = "[" + str(command_type) + "]"
+            command_text = "[" + str(command) + "]"
+            buffer_text  = "[" + str(buffer_name) + "]"
+            value_text = str(value)
             command_type_text = command_type_text.ljust(28) if ljust else command_type_text
             command_text = command_text.ljust(28) if ljust else command_text
             buffer_text = buffer_text.ljust(20) if ljust else buffer_text
