86d16d3b203d4d0ed7a41972e5cb50328f4431ce
Willem van Ketwich <willvk@gmail.com>
2020-01-19 20:01:51 +1100

    fix tests

--- tests/pytest_tests/test_action.py
+++ tests/pytest_tests/test_action.py
@@ -8,11 +8,12 @@ import tide.plugin.action as ACTION
 
 def test__get_actions_list__has_correct_count():
     actions_list = ACTION.__get_actions_list()
-    assert len(actions_list) == 11
+    assert len(actions_list) == 12
 
 def test__get_actions_list__has_expected_actions():
     actions_list = ACTION.__get_actions_list()
     assert set(['run_config_command', 'run_command_with_match', 'run_command',
                 'get_current_buffer_line', 'run_editor_function', 'run_command_string',
                 'print_debug', 'display_template', 'get_current_buffer_name',
-                'run_python_function', 'set_var']) == set(actions_list)
+                'run_python_function', 'set_var', 'run_command_with_match_group']
+            ) == set(actions_list)
