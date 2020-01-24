1aa84bf00d3f7a7e1caff0a2a3481372152fb07c
Willem van Ketwich <willvk@gmail.com>
2020-01-25 10:53:26 +1100

    adjust col width

--- tide/print_to_stdout.py
+++ tide/print_to_stdout.py
@@ -11,7 +11,7 @@ class PrintToStdout:
             command_text = "[" + str(command) + "]"
             buffer_text = "[" + str(buffer_name) + "]"
             value_text = str(value)
-            command_type_text = command_type_text.ljust(28) if ljust else command_type_text
+            command_type_text = command_type_text.ljust(30) if ljust else command_type_text
             command_text = command_text.ljust(28) if ljust else command_text
             buffer_text = buffer_text.ljust(20) if ljust else buffer_text
             value_text = (value_text[:110] + '..') if len(value_text) > 110 else value_text
@@ -28,8 +28,8 @@ class PrintToStdout:
             raw_value_string = str(raw_value)
             raw_value_text = (raw_value_string[:75] + '..') if len(raw_value_string) > 75 else raw_value_string
             if ljust:
-                filter_type_text = filter_type_text.ljust(28)
-                process_or_match_text = process_or_match_text.ljust(28)
+                filter_type_text = filter_type_text.ljust(30)
+                process_or_match_text = process_or_match_text.ljust(30)
                 process_or_match_on_text = process_or_match_on_text.ljust(28)
                 result_text = result_text.ljust(10)
             print(heading_text, filter_type_text, process_or_match_text, process_or_match_on_text, result_text, raw_value_text)
