f0e0e2c46099eacc80b01681afaa4e7efaa41979
Willem van Ketwich <willvk@gmail.com>
2020-02-18 21:10:30 +1100

    make run_config use interpolation

--- tide/defaults/actions/run_command.py
+++ tide/defaults/actions/run_command.py
@@ -1,11 +1,13 @@
 from tide.plugin.action_base import action_base
 from tide.command.command_handler import CommandHandler
 from tide.print_to_stdout import PrintToStdout as PTS
+import tide.utils.interpolate as Interpolate
 
 class run_command(action_base):
 
     def run(self, command_item, buffer_name=''):
         buffer_name = buffer_name or command_item.get("buffer_name", '')
-        command_item_name = command_item['command']
-        PTS.info("RUN_COMMAND", command_item_name, buffer_name, command_item)
-        return CommandHandler().run_command(command_item['command'], buffer_name)
+        command_value = command_item['command']
+        interpolated_command_value = Interpolate.interpolate_variables(command_value)
+        PTS.info("RUN_COMMAND", interpolated_command_value, buffer_name, command_item)
+        return CommandHandler().run_command(interpolated_command_value, buffer_name)
